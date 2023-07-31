b8afc423e5593340628223a27bf7006dfd32814b
Salih Öğüt <79373396+CrincklyRocket@users.noreply.github.com>
2023-07-31 19:31:33 +0300

    Delete Assigment+_10 directory

--- Assigment+_10/Query_1.sql
+++
@@ -1,4 +0,0 @@
-SELECT country.country, city.city FROM city   
-LEFT JOIN country  ON country.country_id = city.country_id;
-
-
--- Assigment+_10/Query_2.sql
+++
@@ -1,4 +0,0 @@
-SELECT payment.payment_id, customer.first_name, customer.last_name FROM customer    
-RIGHT JOIN payment ON payment.customer_id = customer.customer_id;
-
-
